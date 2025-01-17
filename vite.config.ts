5172547ad5261d9f3a1730e9822132f175c6b976
YevhenVe <eugene.veprytskyi@gmail.com>
2025-01-16 21:23:57 -0500

    sitemap, robots

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import Pages from 'vite-plugin-pages';
+import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   build: {
     outDir: 'build',
     emptyOutDir: true,
   },
-  plugins: [react(), svgr()],
+  plugins: [
+    react(), 
+    svgr(), 
+    Pages(), 
+    Sitemap({
+      hostname: 'https://yevhen-portfolio-page.web.app',
+    }),
+  ],
   server: {
     watch: {
       usePolling: true,
